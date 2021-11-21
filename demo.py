09ae9bc7d77c50786c314fe6789695516e614b01
dinhson <19522132@gm.uit.edu.vn>
2021-11-21 22:13:02 +0700

    fix img.shape != None

--- demo.py
+++ demo.py
@@ -4,21 +4,23 @@ import os, cv2
 import argparse
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-
 # nếu không có if __main__ thì kết quả chỉ được hiển thị toàn bộ sau khi train xong
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--img_path', type=str, default="img.png", help='-')
-    parser.add_argument('--ratio', type=int, default=2, help='-')
+    parser.add_argument('--scale', type=int, default=2, help='-')
     FLAGS, unparsed = parser.parse_known_args()
 
     tf.compat.v1.disable_eager_execution()
 
     model = SuperResolution()
     model.restore(checkpoint_path="checkpoint/model-ckpt")
+    
+    image_list = os.listdir("demo/images/")
+    for imname in image_list:
+        impath = os.path.join("demo/images", imname)
+        image = cv2.imread(impath)
 
-    img = cv2.imread(FLAGS.img_path)
-    if img.shape != None:
-        model.execute(img, FLAGS.ratio, save=True)
-    else:
-        print(f"{FLAGS.img_path} do not exist")
+        recon_img, bicubic_img = model.predict(image, FLAGS.scale)
+
+        cv2.imwrite(f"demo/results/recon/{imname}_recon.bmp", recon_img)
+        cv2.imwrite(f"demo/results/bicubic/{imname}_bicubic.bmp", bicubic_img)
